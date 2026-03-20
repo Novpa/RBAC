374d99f2d6ef351a18b125bac1db83b9323bc0af
Novpa <aa.novparadiputra@binus.ac.id>
2026-03-20 16:56:47 +0800

    cleaning up inecessary space

--- backend/src/routers/auth.router.ts
+++ backend/src/routers/auth.router.ts
@@ -1,5 +1,4 @@
 import { Request, Response, Router } from "express";
-
 import { authController } from "../controllers/auth.controller";
 import { authentication, authorization } from "../middlewares/auth.middleware";
 
